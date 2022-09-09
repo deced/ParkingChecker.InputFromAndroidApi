c40ddac1d6c680d9965cffde562f48d96a97185b
deced <samofalov.v@tut.by>
2022-09-09 19:23:59 +0300

    health check

---
+++ ParkingChecker.InputFromAndroidApi/Controllers/Input.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ParkingChecker.InputFromAndroidApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class Input : ControllerBase
+    {
+        public string HealthCheck()
+        {
+            return "working";
+        }
+        
+        [HttpPost]
+        public IActionResult SendImage()
+        {
+            return Ok();
+        }
+    }
+}
